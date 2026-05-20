27344d7ff44a8041e0ca22cb2fe86590af06764b
WINA-STAR999 <tranhieu899999@gmail.com>
2026-05-20 16:18:38 +0700

    Add: two-integer-sum - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum/submission-5.cpp
@@ -0,0 +1,16 @@
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map <int, int> mp;
+        int size_nums = nums.size();
+        for (int i = 0; i < size_nums; i++){
+            int y = target - nums[i];
+            if(mp.find(y) != mp.end()){
+                return {mp[y], i};
+            }
+            mp[nums[i]] = i;
+        }
+        return {};
+    }
+};
