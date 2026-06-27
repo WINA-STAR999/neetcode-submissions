b1fb91286ad2d7b981343cdff993887817db39e0
WINA-STAR999 <tranhieu899999@gmail.com>
2026-06-27 16:48:15 +0700

    Add: time-based-key-value-store - submission-6

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-6.cpp
@@ -0,0 +1,34 @@
+class TimeMap {
+public:
+    TimeMap() {
+        
+    }
+    unordered_map <string, vector <pair<int, string>>> mp;
+    
+    void set(string key, string value, int timestamp) {
+        mp[key].push_back({timestamp, value});
+    }
+    
+    string get(string key, int timestamp) {
+        if (mp.find(key) == mp.end()) return "";
+        int i = 0;
+        int j = mp[key].size() - 1;
+        string result = "";
+        vector <pair<int, string>> &arr = mp[key];
+        while (i <= j){
+            int m = i + (j - i) / 2;
+            if (arr[m].first == timestamp){
+                return arr[m].second;
+            }
+            if (arr[m].first > timestamp){
+                j = m - 1;
+            }
+            else{
+                i = m + 1;
+                result = arr[m].second;
+            }
+        }
+        return result;
+    }
+
+};
